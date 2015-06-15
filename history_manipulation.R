06d1c6608bbf518bab03b9129bf942dfce48cad8
apxu-devil <arh.andr@gmail.com>
2015-06-15 19:29:10 +0300

    1.3_DrawSmile
    + Get coef at date
    + Calc smile at fixed strike range
    + Draw chart

--- history_manipulation.R
+++ history_manipulation.R
@@ -4,6 +4,7 @@
 ### test new commit
 
 require(dplyr)
+require(tidyr)
 require(ggplot2)
 source(file = 'C:\\Users\\arhipov\\Documents\\RBackup\\.Structure\\deriv_vxsmile.R')
 
@@ -13,18 +14,27 @@ local( {
   all.data = all.data[-1]
   all.data$tms = as.Date( substr( as.character(all.data$tms), 0, 10 ) )
   
-  at.date = all.data %>% filter(tms==as.Date('2010-09-06'))
+  vx.at.date = all.data %>% filter(tms==as.Date('2010-09-06'))
   
-  sapply( c(1:nrow(at.date)), function(x){
+  smiles = lapply( c(1:nrow(at.date)), function(x){
     
-    strike = at.date[x,'fut_price', drop=T]
-    fut    = at.date[x,'fut_price', drop=T]
-    tdays  = at.date[1,'t', drop=T]*250
-    coef.vector=as.vector(at.date[x, c('s', 'a', 'b', 'c', 'd', 'e')])
-    vxSmile(strike, fut, tdays, coef.vector)
+    strikes = seq(120000, 170000, 5000)
+    x.row = x
+    sapply(strikes, function(x){
+      
+      strike = x
+      fut    = at.date[x.row,'fut_price', drop=T]
+      tdays  = at.date[1,'t', drop=T]*250
+      coef.vector = as.vector(at.date[x.row, c('s', 'a', 'b', 'c', 'd', 'e')])
+      vxSmile(strike, fut, tdays, coef.vector)
     
-    
+    })
   })
+  
+  names(smiles) = as.vector(vx.at.date$small_name)
+  smiles = gather(data = as.data.frame( c(list(strike = strikes), smiles ) ), key=strike )
+  names(smiles) = c('Strike', 'BaseFutures', 'IV')
+  ggplot(data = smiles, aes(x = Strike, y = IV, color = BaseFutures)) + geom_point() + geom_line()
   
   View(all.data)
 })
